83822ea176708b579e9cdba9b788beec577d0659
BlueNyang <xpsj20@gmail.com>
2025-10-28 02:02:01 +0900

    chore: add component to test configures

--- a11ymarket-web/src/routes/__root.jsx
+++ a11ymarket-web/src/routes/__root.jsx
@@ -1,5 +1,6 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import TestComponent from '@/components/TestComponent';
 
 export const Route = createRootRoute({
   component: Root,
@@ -9,6 +10,7 @@ function Root() {
   return (
     <div>
       <h1>A11yMARKET</h1>
+      <TestComponent />
       <Outlet />
       {import.meta.env.DEV && <TanStackRouterDevtools />}
     </div>
