ce46eaa08ebec50333c53a857589acee9fd00cce
BlueNyang <xpsj20@gmail.com>
2025-10-28 01:59:41 +0900

    refactor: change resolve.alias value into nodejs standard method
    - change to the standard method of nodejs rather than the string method of the vite to ensure stability by creating an absolute path

--- a11ymarket-web/vite.config.js
+++ a11ymarket-web/vite.config.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -19,7 +20,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   test: {
