a7e40d917f0fcff3bab6e3d3c24dd767dee110e6
BlueNyang <xpsj20@gmail.com>
2025-11-17 11:46:37 +0900

    feat: add realtime-ranking section

---
+++ src/components/RealtimeRanking.jsx
@@ -0,0 +1,71 @@
+import { Icon } from '@iconify/react';
+import { Card, CardContent, CardFooter } from './ui/card';
+import { Carousel, CarouselContent, CarouselNext, CarouselPrevious } from './ui/carousel';
+import { useState } from 'react';
+import { Button } from './ui/button';
+import { cn } from '@/lib/utils';
+
+export function RealtimeRanking({ data }) {
+  const [api, setApi] = useState();
+
+  const apiButtonStyles =
+    'absolute top-1/2 size-12 -translate-y-1/2 rounded-full bg-neutral-50 text-neutral-700 shadow-lg hover:bg-sky-100 hover:shadow-xl hover:shadow-sky-100';
+  return (
+    <>
+      <h2 className='mb-4 flex h-16 w-full items-center bg-neutral-300 px-8 text-2xl font-bold'>
+        실시간 인기 상품
+      </h2>
+      <div className='relative flex w-[80%] flex-col items-center justify-center px-8 pb-8'>
+        <Carousel
+          opts={{
+            align: 'center',
+            loop: false,
+          }}
+          className='w-full'
+          setApi={setApi}
+        >
+          <CarouselContent className='gap-8 px-4'>
+            {data?.map((item, _) => (
+              <div className='p-1'>
+                <Card className='overflow-hidden pt-0 transition-transform hover:-translate-y-1 hover:opacity-90 hover:shadow-lg'>
+                  <CardContent className='flex aspect-square w-3xs items-center justify-center p-0'>
+                    <div className='bg-neutral-'></div>
+                    <div className='h-full w-full bg-neutral-400'></div>
+                  </CardContent>
+                  <CardFooter className='flex flex-col items-start gap-2 p-4'>
+                    <span className='text-lg font-semibold'>{item.title}</span>
+                    <span className='text-md font-bold text-neutral-600'>{item.price}원</span>
+                  </CardFooter>
+                </Card>
+              </div>
+            ))}
+          </CarouselContent>
+        </Carousel>
+        <Button
+          variant='outline'
+          size='icon'
+          className={cn(apiButtonStyles, '-left-12')}
+          onClick={() => api?.scrollPrev()}
+          aria-label='이전 상품 보기'
+        >
+          <Icon
+            icon='mdi:chevron-left'
+            className='size-8'
+          />
+        </Button>
+        <Button
+          variant='outline'
+          size='icon'
+          className={cn(apiButtonStyles, '-right-12')}
+          onClick={() => api?.scrollNext()}
+          aria-label='다음 상품 보기'
+        >
+          <Icon
+            icon='mdi:chevron-right'
+            className='size-8'
+          />
+        </Button>
+      </div>
+    </>
+  );
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,5 @@
 import { MainCarousel } from '@/components/MainCarousel';
+import { RealtimeRanking } from '@/components/RealtimeRanking';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
@@ -6,12 +7,25 @@ export const Route = createFileRoute('/')({
 });
 
 function HomePage() {
-  const demoData = [
+  const bannerData = [
     { id: 1, title: '혜택 배너 1', description: '첫 번째 혜택 배너 설명' },
     { id: 2, title: '혜택 배너 2', description: '두 번째 혜택 배너 설명' },
     { id: 3, title: '혜택 배너 3', description: '세 번째 혜택 배너 설명' },
     { id: 4, title: '혜택 배너 4', description: '네 번째 혜택 배너 설명' },
     { id: 5, title: '혜택 배너 5', description: '다섯 번째 혜택 배너 설명' },
+  ];
+
+  const realtimeRankingData = [
+    { id: 1, title: '인기 상품 1', price: 10000 },
+    { id: 2, title: '인기 상품 2', price: 20000 },
+    { id: 3, title: '인기 상품 3', price: 30000 },
+    { id: 4, title: '인기 상품 4', price: 40000 },
+    { id: 5, title: '인기 상품 5', price: 50000 },
+    { id: 6, title: '인기 상품 6', price: 60000 },
+    { id: 7, title: '인기 상품 7', price: 70000 },
+    { id: 8, title: '인기 상품 8', price: 80000 },
+    { id: 9, title: '인기 상품 9', price: 90000 },
+    { id: 10, title: '인기 상품 10', price: 100000 },
   ];
 
   return (
@@ -23,7 +37,13 @@ function HomePage() {
         className='flex h-fit w-full items-center justify-center bg-amber-50 px-4 pb-8 lg:py-8'
         aria-label='주요 혜택 및 이벤트 배너 영역'
       >
-        <MainCarousel data={demoData} />
+        <MainCarousel data={bannerData} />
+      </section>
+      <section
+        className='flex w-full flex-col items-center justify-center'
+        aria-label='실시간 인기 상품 영역'
+      >
+        <RealtimeRanking data={realtimeRankingData} />
       </section>
     </main>
   );
